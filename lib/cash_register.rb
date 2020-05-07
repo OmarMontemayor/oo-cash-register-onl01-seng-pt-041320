18a9abc73dc1edf1586103411071d7c5d6f22045
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-05-07 00:44:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,9 @@
 class CashRegister
-  def initialize(employee_discout=20)
+  def initialize(employee_discout = 20)
     @total = 0
   end
   
-  def discount(employee_discout=20)
+  def discount(employee_discout = 20)
     
   end
 end
