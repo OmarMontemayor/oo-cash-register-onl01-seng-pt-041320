0d91c7e4a2c0ef777d9c8a5b2d94c19f394785af
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-05-07 00:51:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,4 +8,9 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     @total += (price * quantity)
   end
+  
+  def apply_discount
+    calc_discount = @total * (@discount / 100)
+    @total -= calc_discount
+  end
 end
